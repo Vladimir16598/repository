dd376a5518ff5257ae490410e96beb833f62ee4a
Vladimir16598 <kimvladimir007@gmail.com>
2018-02-03 18:11:09 +0600

    1
    creating simple project of flappy

---
+++ trash/algotest/acmp/goldofabba.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class goldofabba
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+
+    String a = sc.next();
+    String b = sc.next();
+    String c = sc.next();
+
+
+    
+  }
+}
